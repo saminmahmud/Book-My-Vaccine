73e7a2c93c53676d5e6c855788a69ab9c9dca8ad
saminmahmud <samin.mahmud18@gmail.com>
2025-02-27 18:12:34 +0600

    update mysite urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mysite import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    
+    path('admin/', admin.site.urls),    
     path('', views.index, name='index'),
     path('center/', include('center.urls', namespace='center')),
     path('vaccine/', include('vaccine.urls', namespace='vaccine')),
@@ -14,8 +18,6 @@ urlpatterns = [
     path('vaccination/', include('vaccination.urls', namespace='vaccination')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Book My Vaccine"
 admin.site.site_title = "Book My Vaccine"
